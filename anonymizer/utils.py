167408bec9b378d8e9490670910ba42f3c5696ce
David Stensland <me@terite.com>
2017-04-05 12:07:30 -0700

    use only values of module dict

--- anonymizer/utils.py
+++ anonymizer/utils.py
@@ -8,19 +8,19 @@ def get_anonymizers(app_config):
     mod = importlib.import_module(anonymizers_module)
 
     anonymizers = []
-    for k, v in mod.__dict__.items():
-        if v is Anonymizer:
+    for value in mod.__dict__.values():
+        if value is Anonymizer:
             continue
 
         is_anonymizer = False
         try:
-            if issubclass(v, Anonymizer) and v.model is not None:
+            if issubclass(value, Anonymizer) and value.model is not None:
                 is_anonymizer = True
         except TypeError:
             continue
 
         if is_anonymizer:
-            anonymizers.append(v)
+            anonymizers.append(value)
 
     anonymizers.sort(key=lambda c: c.order)
     return anonymizers
