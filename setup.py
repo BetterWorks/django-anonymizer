7367a880389441bb7234b9f35c8cd9ba5c405e7c
Donnie Ellstrom <dsvene@gmail.com>
2025-06-18 11:34:08 -0700

    chore(update-version): Update version to 0.5.0.18

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*rnames):
 
 setup(
     name="django-anonymizer",
-    version='0.5.0.16-bw',
+    version='0.5.0.18',
     packages=find_packages(exclude=['*.tests']),
     include_package_data=True,
 
